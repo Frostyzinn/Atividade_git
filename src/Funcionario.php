41ca5f0b213523f2f7ed2ab1a3f0527e41d2000d
emanuelyan89@gmail.com <joao@email.com>
2026-06-23 12:04:30 -0300

    Classes criadas

---
+++ src/Funcionario.php
@@ -0,0 +1,31 @@
+<?php
+
+class Funcionario {
+    private $nome;
+    private $idade;
+
+    public function __construct($nome, $idade) {
+        $this->nome = $nome;
+        $this->idade = $idade;
+    }
+
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function getIdade() {
+        return $this->idade;
+    }
+
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    public function setIdade($idade) {
+        $this->idade = $idade;
+    }
+
+  
+}
+
+?>
